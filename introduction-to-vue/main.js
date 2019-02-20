e7fc7e12e05b78d958b5aced7cae7ca847d25422
John Cyrill <johncyril@gorated.ph>
2019-02-20 16:51:50 +0800

    Chapter 7 done

--- introduction-to-vue/main.js
+++ introduction-to-vue/main.js
@@ -1,20 +1,29 @@
 var app = new Vue({
   // OPTIONS
-  el: '#app', // element to connect with this instance
-  data: { // set data
+  el: "#app", // element to connect with this instance
+  data: {
+    // set data
+    brand: "Ika Musume",
     product: "Squiddy Doll",
-    image: "../assets/images/ika-musume.jpg",
+    selectedVariant: 0,
     link: "https://jccultima123.github.io",
-    inStock: true,
+    // inStock: true,
+    cart: 0,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "../assets/images/ika-musume-chibi.png",
+        variantQuantity: 10,
+        variantOnSale: false
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "../assets/images/ika-musume-chibi-2.jpg",
+        variantQuantity: 0,
+        variantOnSale: true
       }
     ],
     sizes: [
@@ -28,6 +37,34 @@ var app = new Vue({
       }
     ],
     inventory: 100,
-    onSale: true
+    // onSale: true
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    removeToCart: function() {
+      this.cart -= 1;
+    },
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    }
+  },
+  // get computed functions 
+  // (get direct computed value)
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].variantOnSale;
+    }
   }
-})
+});
